35e117b7a0d9c4f70fa002add136fd23ba1c532e
Makc-Ko <131991427+Makc-Ko@users.noreply.github.com>
2025-11-15 09:33:03 +0200

    Initial commit with Animaru extension

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Animaru extension installed!");
+});
---
+++ content.js
@@ -0,0 +1,2 @@
+console.log("Animaru content script active on anitube.ua");
+document.body.style.backgroundColor = "#f0f0f0";
